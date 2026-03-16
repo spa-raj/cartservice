3cd7fb1b6bb17265cc621d255e841292a0258caa
Sparsh <sparsh.raj30@gmail.com>
2026-03-16 19:40:10 +0530

    Add Kafka producer for cart events
      - CartEventType enum, CartEvent model, KafkaTopics constant
      - CartEventProducer with fire-and-forget pattern
      - Integrated into CartServiceImpl for all cart operations
      - userId as Kafka key for per-user ordering
    
      Fixes #4
    
    Signed-off-by: Sparsh <sparsh.raj30@gmail.com>

---
+++ src/main/java/com/vibevault/cartservice/constants/KafkaTopics.java
@@ -0,0 +1,8 @@
+package com.vibevault.cartservice.constants;
+
+public final class KafkaTopics {
+    public static final String CART_EVENTS = "cart-events";
+
+    private KafkaTopics() {
+    }
+}
