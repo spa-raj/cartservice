02f94eace0b1e0e624e3962c8a5c4ebb1f477b81
Sparsh <sparsh.raj30@gmail.com>
2026-03-17 18:35:21 +0530

    Fix RedisConfig: use Jackson 3 ObjectMapper for GenericJacksonJsonRedisSerializer
    Signed-off-by: Sparsh <sparsh.raj30@gmail.com>

---
+++ src/main/java/com/vibevault/cartservice/configurations/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.vibevault.cartservice.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import tools.jackson.databind.ObjectMapper;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    public static final Duration CART_CACHE_TTL = Duration.ofMinutes(30);
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJacksonJsonRedisSerializer(new ObjectMapper()));
+        return template;
+    }
+}
